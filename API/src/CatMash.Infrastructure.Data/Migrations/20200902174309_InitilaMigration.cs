9804ad387c567e32e8722c48d2197d2f1c798500
Naoufel Bellali <naoufel.bellali@external.total.com>
2020-09-02 19:46:54 +0200

    add entity framework and generate local db

---
+++ API/src/CatMash.Infrastructure.Data/Migrations/20200902174309_InitilaMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatMash.Infrastructure.Data.Migrations
+{
+    public partial class InitilaMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cats",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Url = table.Column<string>(maxLength: 1000, nullable: false),
+                    Score = table.Column<int>(nullable: false, defaultValueSql: "0")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cats", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cats");
+        }
+    }
+}
