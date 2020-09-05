84a372ac09f7b7823f419b0835dfd7f0bc71c4cb
Naoufel Bellali <naoufel.bellali@external.total.com>
2020-09-05 16:54:00 +0200

    get paged result

--- API/src/CatMash.Domain.Interface/Repository/ICatRepository.cs
+++ API/src/CatMash.Domain.Interface/Repository/ICatRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CatMash.Domain.Entities.DTO;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace CatMash.Domain.Interface.Repository
@@ -6,5 +7,6 @@ namespace CatMash.Domain.Interface.Repository
     public interface ICatRepository : IRepository<Cat>
     {
         Task<Cat> GetByIdAsync(string id);
+        Task<IEnumerable<Cat>> GetOrdredAndPagedCatsAsync(int pageNumber, int pageSize);
     }
 }
